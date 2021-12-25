590d182129251c67ae75debb9fbe9f5c5d42d222
Maxime PERRIN-LIVENAIS <maxime.perrin-livenais@epita.fr>
2021-12-25 22:38:21 +0100

    Vertices not hardcoded in shaders anymore
    Signed-off-by: Maxime PERRIN-LIVENAIS <maxime.perrin-livenais@epita.fr>

--- src/scene/scene_builder.cc
+++ src/scene/scene_builder.cc
@@ -21,35 +21,27 @@ std::shared_ptr<Scene> build_scene(misc::Options& options)
         return nullptr;
     }
 
-
     {
-        auto points = std::vector<float>({-0.5, 0.0, 0.5,
-                                            0.5, 0.0, 0.5,
+        auto points = std::vector<float>({-1.0, 0.5, 1.0,
+                                            -0.5, 0.5, -1.0,
+                                            0.5, 0.5, -1.0,
+                                            1.0, 0.5, -1.0,
+                                            -1.0, 0.0, -0.5,
+                                            -0.5, 0.0, -0.5,
                                             0.5, 0.0, -0.5,
-                                            -0.5, 0.0, -0.5});
+                                            1.0, 0.0, -0.5,
+                                            -1.0, 0.0, 0.5,
+                                            -0.5, 0.0, 0.5,
+                                            0.5, 0.0, 0.5,
+                                            1.0, 0.0, 0.5,
+                                            -1.0, -0.5, 1.0,
+                                            -0.5, 0.3, 1.0,
+                                            0.5, 0.3,  1.0,
+                                            1.0, 0.3,  1.0});
 
         auto fun = [](){ glPatchParameteri(GL_PATCH_VERTICES, 16); glDrawArrays(GL_PATCHES, 0, 16); };
 
-        auto object = Object::build_new_object(points, 1, program, fun);
-
-        /*
-        auto control_points = std::vector<float>(-1.0, 0.5, 1.0,
-                                                    -0.5, 0.5, -1.0,
-                                                    0.5, 0.5, -1.0,
-                                                    1.0, 0.5, -1.0,
-                                                    -1.0, 0.0, -0.5,
-                                                    -0.5, 0.0, -0.5,
-                                                    0.5, 0.0, -0.5,
-                                                    1.0, 0.0, -0.5,
-                                                    -1.0, 0.0, 0.5,
-                                                    -0.5, 0.0, 0.5,
-                                                    0.5, 0.0, 0.5,
-                                                    1.0, 0.0, 0.5,
-                                                    -1.0, -0.5, 1.0,
-                                                    -0.5, 0.3, 1.0,
-                                                    0.5, 0.3,  1.0,
-                                                    1.0, 0.3,  1.0)
-        */
+        auto object = Object::build_new_object(points, 3, program, fun);
 
         scene->add_object(object);
     }
